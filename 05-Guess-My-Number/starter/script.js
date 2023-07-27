d452c707a1a8ebb35dd8855c25dbfa1ccee791f8
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2023-07-27 23:54:51 +0900

    스코프체인, this, 호이스팅

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// 1. 1 부터 20 사이의 랜덤숫자를 뽑기
+// 2. Check! 버튼 누르면 Score 숫자 -1
+// 3. Again! 버튼 누르면 Score 숫자 20으로 초기화 1부터 20사이 랜덤 다시뽑기
+// 4. 입력한 숫자와 뽑은 랜덤 숫자가 맞을 경우 정답!
+
+const againBtn = document.querySelector('.again');
+const checkBtn = document.querySelector('.check');
+const guessBox = document.querySelector('.guess');
+const score = document.querySelector('.score');
+
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+const handleAgain = () => location.reload();
+const handleCheck = () => {
+  if (!guessBox.value) {
+    return alert('숫자를 입력해주세요!');
+  }
+  if (Number(score.innerText) === 0) {
+    return alert('실패! Again! 버튼을 눌러 다시 시작해주세요!');
+  }
+
+  score.innerText = score.innerText - 1;
+  if (randomNumber > Number(guessBox.value)) {
+    return alert('숫자가 작아요!');
+  }
+  if (randomNumber < Number(guessBox.value)) {
+    return alert('숫자가 커요!');
+  }
+  if (randomNumber === Number(guessBox.value)) {
+    alert('성공!!');
+    return location.reload();
+  }
+};
+againBtn.addEventListener('click', handleAgain);
+checkBtn.addEventListener('click', handleCheck);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,106 @@
 'use strict';
+
+// 스코프 체인
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
+//   function printAge() {
+//     console.log(`나의 나이는 ${age} 입니다`);
+//     if (birthYear === 1998) {
+//       var name = '김민겸';
+//     }
+//     console.log(name);
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// const firstName = '김민겸';
+
+// calcAge(1998);
+
+// 호이스팅 & TDZ
+// console.log(name);
+// console.log(add(1, 2));
+// // console.log(age);
+// // console.log(job);
+
+// var name = '김민겸';
+// let age = 25;
+// const job = 'student';
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // Example
+
+// console.log(numProduct);
+
+// if (!numProduct) deleteCart();
+
+// var numProduct = 10;
+
+// console.log(numProduct);
+
+// function deleteCart() {
+//   console.log('장바구니 모두 삭제');
+// }
+
+// this에 대해서
+// console.log(this);
+
+// function calcAge(birthYear) {
+//   console.log(2023 - birthYear);
+//   console.log(this);
+// }
+
+// calcAge(1998);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2023 - birthYear);
+//   console.log(this);
+// };
+
+// calcAgeArrow(1998);
+
+// const man = {
+//   year: 1998,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2023 - this.year);
+//   },
+// };
+
+// const woman = {
+//   year: 2000,
+// };
+
+// man.calcAge();
+
+// woman.calcAge = man.calcAge;
+
+// woman.calcAge();
+
+// const add = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+
+// add(2, 3);
+
+let lastName = '김민겸';
+let oldLastName = lastName;
+lastName = '홍길동';
+
+console.log(lastName, oldLastName);
+
+const man = {
+  name: '김민겸',
+  age: 25,
+  family: ['엄마', '아빠'],
+};
+
+const woman = Object.assign({}, man);
+
+console.log(woman === man);
